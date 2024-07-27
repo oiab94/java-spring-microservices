19851e02cb7b0535a5f5a64473018eb94cbedf1b
Oscar <oscar.alonso.994@gmail.com>
2024-07-27 18:59:31 -0400

    Dto de la tabla Products

---
+++ src/main/java/org/oiab/productservice/dtos/ProductDto.java
@@ -0,0 +1,13 @@
+package org.oiab.productservice.dtos;
+
+import java.math.BigDecimal;
+
+public class ProductDto {
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private BigDecimal price;
+}
