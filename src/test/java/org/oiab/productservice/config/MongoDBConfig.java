1e2daabae91b7602fb55abb4d3923d24c9b5e962
Oscar <oscar.alonso.994@gmail.com>
2024-08-17 13:16:15 -0400

    Removemos variable innecesaria

--- src/test/java/org/oiab/productservice/config/MongoDBConfig.java
+++ src/test/java/org/oiab/productservice/config/MongoDBConfig.java
@@ -1,20 +1,29 @@
 package org.oiab.productservice.config;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 
 @Testcontainers
-@AllArgsConstructor
 @Slf4j
 public class MongoDBConfig {
 	@Container
-	 static final MongoDBContainer MY_MONGO_CONTAINER = new MongoDBContainer("mongo:latest");
+	 static  MongoDBContainer MY_MONGO_CONTAINER = new MongoDBContainer("mongo:latest");
+
+	@DynamicPropertySource
+	static void containerProperties(DynamicPropertyRegistry registry) {
+		log.info("ADD ContainerProperties");
+		registry.add("spring.data.mongodb.uri", MY_MONGO_CONTAINER::getReplicaSetUrl);
+		registry.add("spring.data.mongodb.database", () -> "products-test");
+
+		log.info("URI: " + MY_MONGO_CONTAINER.getReplicaSetUrl());
+	}
 
 	@BeforeAll
 	static void startMongoDB() {
